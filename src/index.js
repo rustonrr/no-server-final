9c112a6a3af962d37ccf32abe8be20fb8ffdb240
Ruston Reformado <rustonreformado@gmail.com>
2017-06-24 16:36:33 -0600

    created store and reducer

--- src/index.js
+++ src/index.js
@@ -3,18 +3,21 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
-import Hello from "./hello/hello";
+// import Hello from "./hello/hello";
 import { MemoryRouter as Router, Route, Switch } from "react-router-dom";
-// import { Provider } from "react-redux";
+import { Provider } from "react-redux";
 import hello from "./hello/hello";
+import store from "./redux/store";
 
 ReactDOM.render(
-    <Router>
-        <Switch>
-            <Route exact path="/" component={App} /> 
-            <Route path="/hello" component={hello} />
-        </Switch>
-    </Router>,
+    <Provider store={store}>
+        <Router>
+            <Switch>
+                <Route exact path="/" component={App} /> 
+                <Route path="/hello" component={hello} />
+            </Switch>
+        </Router>
+    </Provider>,
 document.getElementById('root')
 );
 registerServiceWorker();
