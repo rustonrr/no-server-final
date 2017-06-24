8e69b66f63912a590e43451bcc813f12c328dc61
Ruston Reformado <rustonreformado@gmail.com>
2017-06-24 15:37:21 -0600

    set up hello component

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import Hello from "./hello/hello";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<App />, document.getElementById('root')
+);
 registerServiceWorker();
